a6239b82480d80f76d6bd0208bb11a18af7d0da9
Ricardo Hinds <hindsricardo@gmail.com>
2018-01-02 10:15:38 -0600

    reseed node_modules

--- dist/index.js
+++ dist/index.js
@@ -14,13 +14,37 @@ var _seraph = require('seraph');
 
 var _seraph2 = _interopRequireDefault(_seraph);
 
-var _accounts = require('../apps/assembly/accounts');
+var _messages = require('../apps/assembly/messages');
+
+var _messages2 = _interopRequireDefault(_messages);
+
+var _conversations = require('../apps/assembly/conversations');
+
+var _conversations2 = _interopRequireDefault(_conversations);
+
+var _plan = require('../apps/buildfit/plan');
+
+var _plan2 = _interopRequireDefault(_plan);
+
+var _user = require('../apps/buildfit/user');
+
+var _user2 = _interopRequireDefault(_user);
+
+var _feedback = require('../apps/buildfit/feedback');
+
+var _feedback2 = _interopRequireDefault(_feedback);
+
+var _analytics = require('../apps/buildfit/analytics');
+
+var _analytics2 = _interopRequireDefault(_analytics);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var port = 3000;
+var neo4j = require('neo4j-driver').v1;
+
+var port = 3001;
 
 var Server = function Server() {
 	_classCallCheck(this, Server);
@@ -32,16 +56,15 @@ var Server = function Server() {
 			user: url.auth.split(':')[0],
 			pass: url.auth.split(':')[1] });
 	} else {
-		this.db = (0, _seraph2.default)({
-			user: "local",
-			pass: "test"
-		});
+		var driver = neo4j.driver("bolt://ec2-54-67-83-66.us-west-1.compute.amazonaws.com:7687", neo4j.auth.basic("neo4j", "Hin81you!"));
+
+		this.db = driver.session();
 	}
 
 	this.server = _restify2.default.createServer();
-	this.server.use(_restify2.default.acceptParser(server.acceptable));
-	this.server.use(_restify2.default.queryParser());
-	this.server.use(_restify2.default.bodyParser());
+	this.server.use(_restify2.default.plugins.acceptParser(this.server.acceptable));
+	this.server.use(_restify2.default.plugins.queryParser());
+	this.server.use(_restify2.default.plugins.bodyParser());
 	this.server.use(function (req, res, next) {
 		res.header('Access-Control-Allow-Origin', "*");
 		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
@@ -52,7 +75,12 @@ var Server = function Server() {
 	this.server.listen(process.env.PORT || port, function () {
 		console.log("Server started @ ", process.env.PORT || port);
 	});
-	this.accounts = new _accounts.Accounts(this.db, this.server);
+	this.messagesRouter = new _messages2.default(this.db, this.server);
+	this.conversationsRouter = new _conversations2.default(this.db, this.server);
+	this.planRouter = new _plan2.default(this.db, this.server);
+	this.userRouter = new _user2.default(this.db, this.server);
+	this.feedbackRouter = new _feedback2.default(this.db, this.server);
+	this.analyticsRouter = new _analytics2.default(this.db, this.server);
 };
 
 new Server();
