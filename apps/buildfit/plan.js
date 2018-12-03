ca7182ae4e9a82927344988748e0f5f55162e6d9
Ricardo Hinds <hindsricardo@gmail.com>
2018-12-02 23:38:38 -0500

    fix6

--- apps/buildfit/plan.js
+++ apps/buildfit/plan.js
@@ -1860,7 +1860,7 @@ class Plan {
           let now = new Date().getTime();
           let oneWeekMil = 604800000;
           let fourweeksMill = oneWeekMil
-          let cypher2 = "MATCH (n:RESULTS {user:$id, method:$methodID}) WHERE n.created > $now - $oneweek RETURN n ORDER BY n.created DESC LIMIT 1" //TODO need to set a time 7 days ago 21
+          let cypher2 = "MATCH (n:RESULTS {user:$id, methodID:$methodID}) WHERE n.created > $now - $oneweek RETURN n ORDER BY n.created DESC LIMIT 1" //TODO need to set a time 7 days ago 21
           db.run(cypher2, {
             id: body.id,
             methodID:body.methodID,
