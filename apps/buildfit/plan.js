05df2b5389f03772f8b8ff773d4293afa18d7bce
Ricardo Hinds <hindsricardo@gmail.com>
2018-12-25 09:05:38 +0200

    fix

--- apps/buildfit/plan.js
+++ apps/buildfit/plan.js
@@ -1334,7 +1334,6 @@ class Plan {
           x.routine = JSON.parse(x.routine);
           return x;
         });
-        console.log("status", results[0].status)
       if(results.length > 0){ //if there is a previous workout
           if(moment(results[0].created).format("LL") == today || results[0].status != "skipped" && results[0].status != "completed" && results[0].status != "incomplete_finish" && results[0].status != "rest_day"){ // if the previous workout is the same day
             res.writeHead(200, header);
