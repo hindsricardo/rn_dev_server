16b7872da802e6ac4bf60e5d7b9cea34b42d9dfe
Ricardo Hinds <hindsricardo@gmail.com>
2018-12-30 04:26:04 +0200

    fix

--- apps/buildfit/plan.js
+++ apps/buildfit/plan.js
@@ -681,10 +681,10 @@ class Plan {
       let body = req.body;
       let month = 86400000 * 30;
       let now = new Date().getTime();
-      let cypher = "UNWIND $parts AS part MATCH (m:METHOD) WHERE m.descipline = $focus AND part IN m.parts MATCH (t:TRAINER {uuid:m.trainer}) MATCH (methods:METHOD {trainer: t.uuid}) MATCH (result:RESULTS {trainerID:t.uuid}) RETURN t {.*, methods: collect(DISTINCT methods {.duration, .location, .daysAweek, .focus, .methodDescription, .gender, .parts, .descipline, .pattern}), rating: avg(result.score) ORDER BY t.rating DESC LIMIT 2000 " ;
+      let cypher = "UNWIND $parts AS part MATCH (m:METHOD) WHERE m.descipline = $focus AND part IN m.parts MATCH (t:TRAINER {uuid:m.trainer}) MATCH (methods:METHOD {trainer: t.uuid}) MATCH (result:RESULTS {trainerID:t.uuid}) RETURN t {.*, methods: collect(DISTINCT methods {.duration, .location, .daysAweek, .focus, .methodDescription, .gender, .parts, .descipline, .pattern}), rating: avg(result.score)} ORDER BY t.rating DESC LIMIT 2000 " ;
         db.run(cypher, {
           focus: body.focus,
-          
+
           parts: body.parts,
         }).then((results) => {
           db.close();
